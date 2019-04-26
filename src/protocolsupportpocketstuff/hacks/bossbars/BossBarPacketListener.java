ac4a1bee8fc233b608fa073caaefc53c1235ea26
WesleyVanNeck <wesleyvanneck105@outlook.com>
2019-04-26 17:24:52 +0200

    Update BossBarPacketListener.java

--- src/protocolsupportpocketstuff/hacks/bossbars/BossBarPacketListener.java
+++ src/protocolsupportpocketstuff/hacks/bossbars/BossBarPacketListener.java
@@ -3,11 +3,11 @@ package protocolsupportpocketstuff.hacks.bossbars;
 import net.minecraft.server.v1_13_R2.IChatBaseComponent;
 import net.minecraft.server.v1_13_R2.PacketPlayOutBoss;
 import protocolsupport.api.Connection;
-import protocolsupport.protocol.packet.middleimpl.clientbound.play.v_pe.EntityMetadata.PeMetaBase;
+import protocolsupport.protocol.packet.middleimpl.clientbound.play.v_pe.EntityMetadata;
 import protocolsupport.protocol.utils.datawatcher.DataWatcherObject;
 import protocolsupport.protocol.utils.datawatcher.objects.DataWatcherObjectFloatLe;
-import protocolsupport.protocol.utils.datawatcher.objects.DataWatcherObjectSVarLong;
 import protocolsupport.protocol.utils.datawatcher.objects.DataWatcherObjectString;
+import protocolsupport.protocol.utils.types.networkentity.NetworkEntityType;
 import protocolsupport.utils.CollectionsUtils;
 import protocolsupportpocketstuff.api.util.PocketCon;
 import protocolsupportpocketstuff.packet.play.BossEventPacket;
@@ -15,17 +15,16 @@ import protocolsupportpocketstuff.packet.play.EntityDataPacket;
 import protocolsupportpocketstuff.packet.play.EntityDestroyPacket;
 import protocolsupportpocketstuff.packet.play.SetAttributesPacket;
 import protocolsupportpocketstuff.packet.play.SpawnEntityPacket;
-import protocolsupportpocketstuff.util.ChatUtils;
 import protocolsupportpocketstuff.util.ReflectionUtils;
+import protocolsupportpocketstuff.util.StuffUtils;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.UUID;
 
 public class BossBarPacketListener extends Connection.PacketListener {
-
+	
 	private Connection con;
 	private HashMap<Long, CachedBossBar> cachedBossBars = new HashMap<>();
 
@@ -34,19 +33,6 @@ public class BossBarPacketListener extends Connection.PacketListener {
 	private static Field BOSS_ACTION;
 	private static Field BOSS_TITLE;
 	private static Field BOSS_HEALTH;
-
-	// Constants
-	private static final String FAKE_BOSS_ENTITY_ID = "minecraft:pig"; // Entity id for a pig, which we fake spawn
-	
-	private static final int DATA_FLAG_INVISIBLE = 5;
-	private static final int DATA_FLAG_IMMOBILE = 16;
-	private static final int DATA_FLAG_SILENT = 17;
-
-	private static final int METADATA_FLAGS = 0; // Encodes a long
-	private static final int METADATA_NAMETAG = 4; // Encodes a string
-	private static final int METADATA_SCALE = 38; // Encodes a float
-	private static final int METADATA_BOUNDING_BOX_WIDTH = 53; // Encodes a float
-	private static final int METADATA_BOUNDING_BOX_HEIGHT = 54; // Encodes a float
 
 	static {
 		try {
@@ -68,6 +54,7 @@ public class BossBarPacketListener extends Connection.PacketListener {
 		this.con = con;
 	}
 
+
 	@Override
 	public void onPacketSending(PacketEvent event) {
 		super.onPacketSending(event);
@@ -86,9 +73,10 @@ public class BossBarPacketListener extends Connection.PacketListener {
 			IChatBaseComponent title = (IChatBaseComponent) ReflectionUtils.get(BOSS_TITLE, packet);
 			float percentage = ReflectionUtils.getFloat(BOSS_HEALTH, packet);
 
-			CachedBossBar bossBar = new CachedBossBar(unique, ChatUtils.toLegacy(title), percentage);
+			CachedBossBar bossBar = new CachedBossBar(unique, StuffUtils.toLegacy(title), percentage);
 
 			cachedBossBars.put(unique, bossBar);
+
 
 			bossBar.spawn(this);
 			return;
@@ -97,9 +85,9 @@ public class BossBarPacketListener extends Connection.PacketListener {
 			if (!cachedBossBars.containsKey(unique))
 				return;
 
-		 	CachedBossBar bossBar = cachedBossBars.get(unique);
-		 	cachedBossBars.remove(unique);
-		 	bossBar.despawn(this);
+			CachedBossBar bossBar = cachedBossBars.get(unique);
+			cachedBossBars.remove(unique);
+			bossBar.despawn(this);
 			return;
 		}
 		if (action == PacketPlayOutBoss.Action.UPDATE_NAME) {
@@ -109,7 +97,8 @@ public class BossBarPacketListener extends Connection.PacketListener {
 			IChatBaseComponent title = (IChatBaseComponent) ReflectionUtils.get(BOSS_TITLE, packet);
 
 			CachedBossBar bossBar = cachedBossBars.get(unique);
-			bossBar.title = ChatUtils.toLegacy(title);
+			bossBar.title = StuffUtils.toLegacy(title);
+
 
 			bossBar.updateMetadata(this);
 			return;
@@ -136,37 +125,22 @@ public class BossBarPacketListener extends Connection.PacketListener {
 			this.percentage = health;
 		}
 
-		private List<SetAttributesPacket.Attribute> getAttributes(float percentage) {
-			return Arrays.asList(
-				new SetAttributesPacket.Attribute("minecraft:health", 0.0f, 100f,
-					percentage * 100, percentage * 100));
-		}
-
-		private CollectionsUtils.ArrayMap<DataWatcherObject<?>> getMetadata(String title) {
-			// Flag setting inspired by https://github.com/thebigsmileXD/BossBarAPI/blob/master/src/xenialdan/BossBarAPI/API.php
-			CollectionsUtils.ArrayMap<DataWatcherObject<?>> metadata = new CollectionsUtils.ArrayMap<>(76);
-
-			long flags = (1 << DATA_FLAG_INVISIBLE) ^ (1 << DATA_FLAG_IMMOBILE) ^ (1 << DATA_FLAG_SILENT);
-			metadata.put(METADATA_FLAGS, new DataWatcherObjectSVarLong(flags));
-			metadata.put(METADATA_NAMETAG, new DataWatcherObjectString(title));
-			metadata.put(METADATA_SCALE, new DataWatcherObjectFloatLe(0.0f));
-			metadata.put(METADATA_BOUNDING_BOX_WIDTH, new DataWatcherObjectFloatLe(0.0f));
-			metadata.put(METADATA_BOUNDING_BOX_HEIGHT, new DataWatcherObjectFloatLe(0.0f));
-			return metadata;
-		}
-
 		public void spawn(BossBarPacketListener listener) {
-			// The magic lies in setting the correct attributes to the spawn packet
-			List<SetAttributesPacket.Attribute> attributes = getAttributes(percentage);
+			CollectionsUtils.ArrayMap<DataWatcherObject<?>> metadata = new CollectionsUtils.ArrayMap<>(76);
+			metadata.put(4, new DataWatcherObjectString(title));
+			metadata.put(39, new DataWatcherObjectFloatLe(0.F)); // scale
+			metadata.put(54, new DataWatcherObjectFloatLe(0.F)); // bb width
+			metadata.put(55, new DataWatcherObjectFloatLe(0.F)); // bb height
 
-			// We propagate the title of the boss bar (the name of the boss) in the metadata
-			CollectionsUtils.ArrayMap<DataWatcherObject<?>> metadata = getMetadata(title);
-
-			SpawnEntityPacket packet = new SpawnEntityPacket(unique, FAKE_BOSS_ENTITY_ID,
-				0, -10, 0, // coordinates
-				0, 0, 0, // motion
-				0, 0, 0, // pitch & yaw
-				attributes, metadata);
+			SpawnEntityPacket packet = new SpawnEntityPacket(
+					unique,
+					NetworkEntityType.PIG,
+					0, -10, 0, // coordinates
+					0, 0, 0, // motion
+					0, 0, // pitch & yaw
+					Arrays.asList(new SetAttributesPacket.Attribute("minecraft:health", 0.0f, 100f, percentage * 100, percentage * 100)),
+					metadata
+			);
 
 			PocketCon.sendPocketPacket(listener.con, packet);
 			PocketCon.sendPocketPacket(listener.con, new BossEventPacket(unique, BossEventPacket.SHOW));
@@ -183,9 +157,10 @@ public class BossBarPacketListener extends Connection.PacketListener {
 		}
 
 		public void updateMetadata(BossBarPacketListener listener) {
-			CollectionsUtils.ArrayMap<DataWatcherObject<?>> metadata = new CollectionsUtils.ArrayMap<>(PeMetaBase.NAMETAG);
-			metadata.put(PeMetaBase.NAMETAG, new DataWatcherObjectString(title));
+			CollectionsUtils.ArrayMap<DataWatcherObject<?>> metadata = new CollectionsUtils.ArrayMap<>(EntityMetadata.PeMetaBase.NAMETAG + 1);
+			metadata.put(EntityMetadata.PeMetaBase.NAMETAG, new DataWatcherObjectString(title));
 			PocketCon.sendPocketPacket(listener.con, new EntityDataPacket(unique, metadata));
+			PocketCon.sendPocketPacket(listener.con, new BossEventPacket(unique, BossEventPacket.UPDATE));
 		}
 
 		public void updateAttributes(BossBarPacketListener listener) {
